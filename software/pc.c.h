04c90b003fa42356cb398f19361c23ac39d8815e
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-09-24 09:55:22 -0300

    Pequenos ajustes.

--- software/pc.c.h
+++ software/pc.c.h
@@ -8,50 +8,50 @@ enum {
 };
 
 void PC_Restart (void) {
-    Serial.print(PC_RESTART);         // codigo de reinicio
+    Serial.print(PC_RESTART);           // codigo de reinicio
     Serial.print(F(";"));
-    Serial.print(S.timeout/1000);     // tempo total de jogo
+    Serial.print(S.timeout/1000);       // tempo total de jogo
     Serial.print(F(";"));
-    Serial.print(S.equilibrio);       // equilibrio ligado?
+    Serial.print(S.equilibrio);         // equilibrio ligado?
     Serial.print(F(";"));
-    Serial.print(S.names[0]);         // atleta a esquerda
+    Serial.print(S.names[0]);           // atleta a esquerda
     Serial.print(F(";"));
-    Serial.print(S.names[1]);         // atleta a direita
+    Serial.print(S.names[1]);           // atleta a direita
     Serial.print(F(";"));
-    Serial.print(S.juiz);             // juiz
+    Serial.print(S.juiz);               // juiz
     Serial.print(F(";"));
     Serial.println();
 }
 
 void PC_Seq (void) {
-    Serial.print(PC_SEQ);             // codigo de nova sequencia
+    Serial.print(PC_SEQ);               // codigo de nova sequencia
     Serial.print(F(";"));
-    Serial.print(G.time/1000);        // tempo jogado em ms
+    Serial.print(G.time/1000);          // tempo jogado em ms
     Serial.print(F(";"));
-    Serial.print(Falls());            // total de quedas
+    Serial.print(Falls());              // total de quedas
     Serial.print(F(";"));
-    Serial.print(S.names[0]);         // atleta a esquerda
+    Serial.print(S.names[0]);           // atleta a esquerda
     Serial.print(F(";"));
-    Serial.print(S.names[1]);         // atleta a direita
+    Serial.print(S.names[1]);           // atleta a direita
     Serial.print(F(";"));
-    Serial.print(S.juiz);             // juiz
+    Serial.print(S.juiz);               // juiz
     Serial.print(F(";"));
     Serial.println();
 }
 
 void PC_Player (int I) {
-    Serial.print(G.jogs[I].total/100);        // pontuacao
+    Serial.print(G.jogs[I].total);      // pontuacao
     Serial.print(F(";"));
     Serial.print(PT_Behind() == I ? 1 : 0);   // 1=atras | 0=ok
     Serial.print(F(";"));
     //Serial.print(G.max_[I]);          // maior velocidade
     //Serial.print(F(";"));
 
-    Serial.print(G.jogs[I].volume/100);    // volume
+    Serial.print(G.jogs[I].volume);     // volume
     Serial.print(F(";"));
-    Serial.print(G.jogs[I].normal);        // media normal
+    Serial.print(G.jogs[I].normal);     // media normal
     Serial.print(F(";"));
-    Serial.print(G.jogs[I].reves);         // media de revez
+    Serial.print(G.jogs[I].reves);      // media de revez
     Serial.print(F(";"));
 
     //Serial.print(normal->tot1);     // total normais
@@ -65,36 +65,36 @@ void PC_Player (int I) {
 }
 
 void PC_Hit (int player, bool is_back, int kmh) {
-    Serial.print(PC_HIT);             // codigo de golpe
+    Serial.print(PC_HIT);               // codigo de golpe
     Serial.print(F(";"));
-    Serial.print(player);             // 0=esquerda | 1=direita
+    Serial.print(player);               // 0=esquerda | 1=direita
     Serial.print(F(";"));
-    Serial.print(is_back);            // 0=normal   | 1=revez
+    Serial.print(is_back);              // 0=normal   | 1=revez
     Serial.print(F(";"));
-    Serial.print(kmh);                // velocidade
+    Serial.print(kmh);                  // velocidade
     Serial.print(F(";"));
     PC_Player(player);
     Serial.println();
 }
 
 void PC_Tick (void) {
-    Serial.print(PC_TICK);            // codigo de tick
+    Serial.print(PC_TICK);              // codigo de tick
     Serial.print(F(";"));
-    Serial.print(G.time/1000);        // tempo jogado em ms
+    Serial.print(G.time/1000);          // tempo jogado em ms
     Serial.print(F(";"));
-    Serial.print(G.total);            // total da dupla
+    Serial.print(G.total);              // total da dupla
     Serial.print(F(";"));
-    Serial.print(G.hits);             // total de golpes
+    Serial.print(G.hits);               // total de golpes
     Serial.print(F(";"));
-    Serial.print((int)G.pace[0]);     // media simples
+    Serial.print((int)G.pace[0]);       // media simples
     Serial.print(F(";"));
     Serial.println();
 }
 
 void PC_Fall (void) {
-    Serial.print(PC_FALL);            // codigo de queda
+    Serial.print(PC_FALL);              // codigo de queda
     Serial.print(F(";"));
-    Serial.print(Falls());            // total de quedas
+    Serial.print(Falls());              // total de quedas
     Serial.print(F(";"));
     PC_Player(0);
     PC_Player(1);
@@ -102,7 +102,7 @@ void PC_Fall (void) {
 }
 
 void PC_End (void) {
-    Serial.print(PC_END);             // codigo de fim
+    Serial.print(PC_END);               // codigo de fim
     Serial.print(F(";"));
     PC_Player(0);
     PC_Player(1);
